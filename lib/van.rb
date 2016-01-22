19a227952aadcb782736e614c75d7155cb6c67b8
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-01-22 16:52:34 +0000

    Finished the van class of ch21

--- lib/van.rb
+++ lib/van.rb
@@ -1,17 +1,18 @@
 require './lib/docking_station.rb'
+require './lib/bike.rb'
 
 
 class Van
 
-  attr_accessor :to_fix
+  attr_reader :to_fix
 
   def dock_bikes_to_fix(broken_bikes)
     @to_fix = []
     @to_fix.push(broken_bikes)
   end
 
-  def release_to_fix
-    
+  def release
+    to_fix.pop(to_fix.length)
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -17,14 +17,34 @@ describe Van do
     expect(subject.dock_bikes_to_fix(array_elements)).to eq [array_elements]
   end
 
-  it {is_expected.to respond_to(:release_to_fix)}
+  it {is_expected.to respond_to(:release)}
 
-  it "releases the bikes" do
-    to_fix = [a,b,c]
-    expect(subject.release_to_fix).to eq (abc)
+  describe "releases the bikes" do
+    station = DockingStation.new
+    bike = Bike.new
+    bike2 = Bike.new
+    bike2.report_broken
+    bike.report_broken
+    station.dock_bike(bike)
+    station.dock_bike(bike2)
+    
+     it 'the array of bikes_to fix in the van should be empty' do
+      subject.dock_bikes_to_fix(station.release_to_fix)
+      subject.release
+      expect(subject.to_fix).to eq([])
+    end
+    
+    it 'should include release the broken bike that was docked' do
+      station0 = DockingStation.new
+      bike0 = Bike.new
+      bike0.report_broken
+      station0.dock_bike(bike0)
+      subject.dock_bikes_to_fix(station0.release_to_fix)
+      expect(subject.release).to eq ([bike0])
+    end
+
+   
 
   end
-
-
 
 end
